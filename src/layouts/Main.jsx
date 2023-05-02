015a0a6b60916b28a800dc634f629a095d4ba270
evo264714 <mh264714@gmail.com>
2023-05-02 14:32:58 +0600

    added a few important components

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../pages/Shared/NavBar/NavBar';
+
+const Main = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+           <p className='text-center'><small>Copyright 2023</small></p>
+        </div>
+    );
+};
+
+export default Footer;
