de442e1f243ef1fa94eb8be2b90ee916ac0ab4b3
evo264714 <mh264714@gmail.com>
2023-05-02 13:01:02 +0600

    installed router and firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyByclkiMsKl_Aqqwc6OrIoDBEyK4VZNhUk",
+  authDomain: "chefs-table-4986c.firebaseapp.com",
+  projectId: "chefs-table-4986c",
+  storageBucket: "chefs-table-4986c.appspot.com",
+  messagingSenderId: "526049112577",
+  appId: "1:526049112577:web:0911f0b694da8407120600"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
